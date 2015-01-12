4941ee2a273f3e38f07a0519c06e663ab8732658
Wataru Sato <wataru@rubyquitous.co.jp>
2015-01-13 00:21:35 +0900

    topデザイン

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,11 @@ class TopController < ApplicationController
   skip_before_filter :authenticate_user
 
   def index
+    @game = Game.last
+    @still = PlanDetail.includes(:plan).
+      where("plans.starts_on >= ?", Date.today ).
+      order('plans.starts_on ASC').limit(4)
+    @news = New.order("id DESC").limit(8)
     render :index, layout: "application"
   end
 
