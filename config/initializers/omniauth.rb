0297a3f1261ba0433339b887c0ae99c00b3ea2f2
Wataru Sato <wataru@rubyquitous.co.jp>
2015-01-13 00:47:27 +0900

    facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :facebook, "667422903356354", "69e822133c06198670a53b1d711a04f8", scope: 'email,user_birthday', display: 'popup'
+    provider :facebook, "733895663384185", "38b98a471451aa0d8d6d4dfc11e6dfff", scope: 'email,user_birthday', display: 'popup'
   else
     provider :facebook, "667422903356354", "69e822133c06198670a53b1d711a04f8", scope: 'email,user_birthday', display: 'popup'
     #OmniAuth.config.on_failure = SessionsController.action(:failure)
